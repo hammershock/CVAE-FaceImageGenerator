dcc19f0aa58c4c2880cb582700a8dc02609d4fff
hammershock <hammershock@163.com>
2024-05-08 01:43:04 +0800

    Removed NumPyTorch from index to add as submodule

--- NumPyTorch/nn/__init__.py
+++
@@ -1,5 +0,0 @@
-from .functional import mse_loss, cross_entropy_loss
-from .utils import one_hot_encode
-from .modules import (Linear, Sequential, SoftmaxLayer, ReLU, Sigmoid, LeakyReLULayer, DropoutLayer, Conv2d, BatchNormLayer,
-                      Flatten, Unflatten, CrossEntropyLoss, Parameter, Module, ConvTranspose2d, AdaptiveAvgPool2d)
-
--- NumPyTorch/nn/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def one_hot_encode(n, num_features) -> np.ndarray:
-    batch_size = n.shape[0]
-    one_hot_vector = np.zeros((batch_size, num_features, 1))
-    for i, item in enumerate(n):
-        one_hot_vector[i, item, :] = 1.
-    return one_hot_vector
